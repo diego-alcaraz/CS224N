2745eab5b31b5f6aa167092cb68edf87de3aa95e
Diego Alcaraz <diazapataal@unal.edu.co>
2025-12-03 14:20:58 +1000

    Part2 A1

---
+++ datalayer_CK.js
@@ -0,0 +1,14 @@
+window.dataLayer.push({
+  event: "form",
+  form_action: "form_submit",
+  form_id: "form_free_audit | form_free_consult | form_free_contribution | form_free_conversion_guide",
+  
+  submitted_fields: {
+    ck_first_name: "Test",
+    ck_last_name: "Test",
+    ck_email: "test@test.com",
+    ck_company_type: "Charity",
+    ck_traffic: "50,000 – 100,000",
+    ck_message: "Testing message"
+  }
+});

365d595e1c7bbefc461f8bf5cb7e9b15d63c9783
Diego Alcaraz <diazapataal@unal.edu.co>
2025-12-10 16:20:29 +1000

    DataLayer

--- datalayer_CK.js
+++ datalayer_CK.js
@@ -1,6 +1,22 @@
+dataLayer.push({ submitted_fields: null });  // Clear the previous submitted_fields object
+
 window.dataLayer.push({
-  event: "form",
-  form_action: "form_submit",
+  event: "form_start",
+  form_id: 'STRING', //Example: form_free_audit | form_free_consult | form_free_contribution | form_free_conversion_guide,
+  
+  submitted_fields: {
+    ck_first_name: "STRING",
+    ck_last_name: "STRING",
+    ck_email: "STRING",
+    ck_company_type: "STRING",
+    ck_traffic: "STRING",
+    ck_message: "STRING"
+  }
+});
+
+
+window.dataLayer.push({
+  event: "form_submit",
   form_id: "form_free_audit | form_free_consult | form_free_contribution | form_free_conversion_guide",
   
   submitted_fields: {
@@ -12,3 +28,4 @@ window.dataLayer.push({
     ck_message: "Testing message"
   }
 });
+
